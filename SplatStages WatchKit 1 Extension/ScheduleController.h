e8c617f00b5d7bef71e869e865a032ffd48c5efe
OatmealDome <theoatmealdome@gmail.com>
2015-11-21 17:48:04 -0500

    Watch: Add basic interface for schedules

---
+++ SplatStages WatchKit 1 Extension/ScheduleController.h
@@ -0,0 +1,14 @@
+//
+//  ScheduleController.h
+//  SplatStages
+//
+//  Created by mac on 2015-11-21.
+//  Copyright © 2015 OatmealDome. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface ScheduleController : WKInterfaceController
+
+@end
---
+++ SplatStages WatchKit 1 Extension/ScheduleController.m
@@ -0,0 +1,41 @@
+//
+//  ScheduleController.m
+//  SplatStages
+//
+//  Created by mac on 2015-11-21.
+//  Copyright © 2015 OatmealDome. All rights reserved.
+//
+
+#import <SplatStagesFramework/SplatDataFetcher.h>
+#import <SplatStagesFramework/SplatUtilities.h>
+#import "ScheduleController.h"
+
+@interface ScheduleController ()
+
+@end
+
+@implementation ScheduleController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+    
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+    
+    // Update schedule data.
+    
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
