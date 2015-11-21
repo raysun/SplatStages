36248c631056fd12083bfe1dee816ca8509932f3
OatmealDome <theoatmealdome@gmail.com>
2015-11-21 12:17:57 -0500

    Watch: Add targets for watchOS 1

---
+++ SplatStages WatchKit 1 Extension/InterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  InterfaceController.m
+//  SplatStages WatchKit 1 Extension
+//
+//  Created by mac on 2015-11-21.
+//  Copyright © 2015 OatmealDome. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController()
+
+@end
+
+
+@implementation InterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
---
+++ SplatStages WatchKit 1 Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  SplatStages WatchKit 1 Extension
+//
+//  Created by mac on 2015-11-21.
+//  Copyright © 2015 OatmealDome. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
