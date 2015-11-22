f86e77d125d88a4043d9417286b6924a612a4bdb
OatmealDome <theoatmealdome@gmail.com>
2015-11-22 12:17:17 -0500

    Framework: Add getFriendsList, checkIfLoggedIn to SplatSquidRingHelper and add more functionality to the loginToSplatNet method

--- SplatStagesFramework/SplatSquidRingHelper.h
+++ SplatStagesFramework/SplatSquidRingHelper.h
@@ -8,6 +8,10 @@
 
 @interface SplatSquidRingHelper : NSObject
 
-+ (void) loginToSplatNet:(void (^)(NSError* error, NSString* when)) errorHandler;
++ (void) loginToSplatNet:(void (^)()) completionHandler errorHandler:(void (^)(NSError* error, NSString* when)) errorHandler;
+
++ (void) getOnlineFriends:(void (^)(NSDictionary* onlineFriends)) completionHandler errorHandler:(void (^)(NSError* error, NSString* when)) errorHandler;
+
++ (void) checkIfLoggedIn:(void (^)(BOOL)) completionHandler errorHandler:(void (^)(NSError* error, NSString* when)) errorHandler;
 
 @end
