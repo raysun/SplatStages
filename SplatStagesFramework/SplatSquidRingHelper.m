3ede4f5a923739511f393d9de3c0956c82f60355
OatmealDome <theoatmealdome@gmail.com>
2015-11-21 22:58:36 -0500

    Framework: Send username and password when trying to log in to SplatNet

--- SplatStagesFramework/SplatSquidRingHelper.m
+++ SplatStagesFramework/SplatSquidRingHelper.m
@@ -25,18 +25,29 @@
 // https://id.nintendo.net/oauth/authorize?client_id=12af3d0a3a1f441eb900411bb50a835a&redirect_uri=https%3A%2F%2Fsplatoon.nintendo.net%2Fusers%2Fauth%2Fnintendo%2Fcallback&response_type=code&state=affc0e17abc5b5af65b9c6a592e5151081771b405be9530e
 
 + (void) loginToSplatNet:(void (^)(NSError* error, NSString* when)) errorHandler {
+    VALValet* valet = [SplatUtilities getValet];
+    if (![valet canAccessKeychain]) {
+        NSDictionary* userInfo = @{
+                                   NSLocalizedDescriptionKey : NSLocalizedString(@"ERROR_KEYCHAIN_ACCESS_DESCRIPTION", nil)
+                                   };
+        NSError* error = [[NSError alloc] initWithDomain:@"me.oatmealdome.ios.SplatStages" code:1 userInfo:userInfo];
+        errorHandler(error, NSLocalizedString(@"ERROR_SPLATNET_LOG_IN", nil));
+    }
+    
     NSURL* url = [NSURL URLWithString:@"https://id.nintendo.net/oauth/authorize"];
     NSMutableURLRequest* request = [[NSMutableURLRequest alloc] initWithURL:url];
     NSDictionary* requestParametersDict = @{
                                         @"client_id" : @"12af3d0a3a1f441eb900411bb50a835a",
                                         @"redirect_uri" : @"https://splatoon.nintendo.net/users/auth/nintendo/callback",
-                                        @"response_type" : @"code"
+                                        @"response_type" : @"code",
+                                        @"username" : [valet stringForKey:@"username"],
+                                        @"password" : [valet stringForKey:@"password"]
                                         };
     NSError* jsonError = nil;
     NSData* requestParameters = [NSJSONSerialization dataWithJSONObject:requestParametersDict options:kNilOptions error:&jsonError];
     
     if (jsonError) {
-        errorHandler(jsonError, @"ERROR_PARSING_SPLATNET_LOG_IN");
+        errorHandler(jsonError, @"ERROR_SPLATNET_LOG_IN");
         return;
     }
     
