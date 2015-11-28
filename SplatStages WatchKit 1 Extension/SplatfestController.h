d6f6104e79032ee98914f052ac71e905443e92f0
OatmealDome <theoatmealdome@gmail.com>
2015-11-28 14:33:12 -0500

    Watch: Add SplatfestController.

---
+++ SplatStages WatchKit 1 Extension/SplatfestController.h
@@ -0,0 +1,14 @@
+//
+//  SplatfestController.h
+//  SplatStages
+//
+//  Created by mac on 2015-11-28.
+//  Copyright © 2015 OatmealDome. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface SplatfestController : WKInterfaceController
+
+@end
---
+++ SplatStages WatchKit 1 Extension/SplatfestController.m
@@ -0,0 +1,36 @@
+//
+//  SplatfestController.m
+//  SplatStages
+//
+//  Created by mac on 2015-11-28.
+//  Copyright © 2015 OatmealDome. All rights reserved.
+//
+
+#import "SplatfestController.h"
+
+@interface SplatfestController ()
+
+@end
+
+@implementation SplatfestController
+
+- (void) awakeWithContext:(id) context {
+    [super awakeWithContext:context];
+    
+    // Configure interface objects here.
+}
+
+- (void) willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void) didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
